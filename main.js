c05357d4b4dea16c01926712a912c86d9be272aa
Nicolas Walcker de Miranda <walcker_@outlook.com>
2020-12-17 15:03:07 -0300

    Initial commit

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const cards = document.querySelectorAll(".card");
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return; 
+        
     this.classList.add('flip'); 
     if (!hasFlippedCard){
         hasFlippedCard = true;
@@ -10,11 +14,48 @@ function flipCard() {
         return;
     }
     secondCard = this;
-    checkForMath();
+    hasFlippedCard = false;
+    checkForMatch();
 }
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disabeCards();
+        return;       
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);  
+    
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500)  
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach((card)=>{
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
 
 cards.forEach((card)=>{
     card.addEventListener('click', flipCard)
-
 })
 
